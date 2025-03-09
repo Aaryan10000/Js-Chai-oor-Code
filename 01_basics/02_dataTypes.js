9f570f0017989694055b79025fe626cd9895af50
Aaryan-Daw <114655216+Aaryan10000@users.noreply.github.com>
2025-03-09 12:37:40 +0000

    datatypes

--- 01_basics/02_dataTypes.js
+++ 01_basics/02_dataTypes.js
@@ -2,5 +2,21 @@
 
 // alert(3+3) //We are using nodejs, not browser
 
-//console.log(3+3); console.log("Hitesh") //!bad practise
+//console.log(3+3); console.log("Aaryan") //!bad practise - code readiblity should be high
 
+let name = "Aaryan"
+let age =18
+let isLoggedIn = false
+
+// number => 2 to power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => value not assigned
+// symbol => to define uniqueness
+
+// object => 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // null
