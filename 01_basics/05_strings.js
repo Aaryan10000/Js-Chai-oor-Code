c95aa47c89004fdec9fc9bf2d3b18332ed682de0
Aaryan-Daw <114655216+Aaryan10000@users.noreply.github.com>
2025-03-18 20:26:29 +0000

    string dataset

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,33 @@
+const name = "Aaryan"
+const repoCount = 40
+
+// console.log(name + repoCount + " Value");
+
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Aaryan-ac-com')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(2));
+// console.log(gameName.indexOf('n'));
+
+const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
+
+const newStringOne = "        Aaryan    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+const url = "https://aaryan.com/aaryan%20dawalkar"
+
+// console.log(url.replace('%20', '-'));
+
+// console.log(url.includes('aaryan'));
+
+console.log(gameName.split('-'));
