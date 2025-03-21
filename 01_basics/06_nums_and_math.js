d54a2b37f051a1a49238d63c800eec37f8ed2690
Aaryan-Daw <114655216+Aaryan10000@users.noreply.github.com>
2025-03-21 19:15:45 +0000

    nums and math concepts

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,39 @@
+const score = 400
+// console.log(score);
+
+
+const balance = new Number(400)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(typeof( balance));
+// console.log(balance.toFixed(1));
+
+const otherNumber = 123.4532
+
+// console.log(otherNumber.toPrecision(4));
+// console.log(otherNumber.toPrecision(2));
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// Number functions -- Min_Value and Max_Value
+
+// ++++++++++++++ Maths ++++++++++++++++
+
+// console.log(Math);
+// console.log(Math.abs(-11));
+// console.log(Math.round(4.3));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.2));
+// console.log(Math.floor(4.9));
+
+// Math functions - pow, sqrt, (min, max - for list, arrays)
+
+// console.log(Math.random());
+// console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+//****** console.log(Math.floor(Math.random() * (max - min +1)) + min)
